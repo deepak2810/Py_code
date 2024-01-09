07586aacb4698362adfe5eafd5830dcd6c8fbe40
Deepak Khanka -X (dkhanka) <deepakkhanka.97@gmail.com>
2024-01-09 23:29:42 +0530

    classes and object example

---
+++ To_do_py/classesandobjects.py
@@ -0,0 +1,20 @@
+#python basic program to determine class and object.
+
+#create class
+
+class Car:
+    name = ""
+    gear = 0
+
+
+#create object of the class.
+    
+scross = Car()
+
+#modify the name attribute.
+
+scross.name = 'Scorpio'
+
+
+print(f'Name: {scross.name}')
+
