2337aeb1ccf6b28ffa91443e5d82bb27c3814d7d
Deepak Khanka -X (dkhanka) <deepakkhanka.97@gmail.com>
2024-01-14 00:35:18 +0530

    ....

---
+++ Loops_based_programs/fl1.py
@@ -0,0 +1,9 @@
+# for loop uses a function named as range to iterate values within a limit.
+
+# print even number between 0 to 100:
+
+for i in range(0,101):
+    if i % 2 == 0:
+        print(i)
+
+
