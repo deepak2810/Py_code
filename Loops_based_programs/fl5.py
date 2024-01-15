e02fb8e7119a968867ca62d3553505eb56e3f32f
Deepak Khanka -X (dkhanka) <deepakkhanka.97@gmail.com>
2024-01-16 00:52:09 +0530

    py code

---
+++ Loops_based_programs/fl5.py
@@ -0,0 +1,13 @@
+# python program to calculate sum of numbers between a given range.
+
+n = int(input('please enter a number ! '))
+
+Sum = 0
+
+for i in range(0,n+1):
+    Sum += i
+
+
+print(Sum)
+
+
